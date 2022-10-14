3ba7193607282c4166f9f205cec716d6ef3bb6fc
Mex777 <fologeaalexandru@gmail.com>
2022-10-14 12:50:30 +0300

    added jwt authentication

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
-const env = require("dotenv").config();
-const db = require("./db");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+require("dotenv").config();
+require("./db");
+require("./auth");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 
 const app = express();
 
@@ -22,24 +25,31 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", passport.authenticate("jwt", { session: false }), (req, res) => {
+  res.json({
+    message: "LOGGED",
+    user: req.user,
+  });
+});
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.json({ error: err });
 });
 
 module.exports = app;
--- db.js
+++ db.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.DB_URL)
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("connected"))
   .catch((err) => console.log(err));
 
 mongoose.connection.on("error", (err) => {
   console.log(err);
 });
+
+mongoose.Promise = global.Promise;
